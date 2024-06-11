cf136da4a3cd259c0beb6dcd258ab6e7d611955d
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-06-11 10:01:00 +0200

    Update purgruen-warenbestand.py

--- purgruen-warenbestand.py
+++ purgruen-warenbestand.py
@@ -5,8 +5,8 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 from datetime import datetime
 
 # Funktion zum Laden der Zuordnungsdatei von Google Drive
-def load_mapping(file_path):
-    mapping_df = pd.read_csv(file_path, dtype={'Original_SKU': str, 'Mapped_SKU': str})
+def load_mapping(url):
+    mapping_df = pd.read_csv(url, dtype={'Original_SKU': str, 'Mapped_SKU': str})
     return mapping_df
 
 # Funktion zum Verarbeiten der hochgeladenen Datei
@@ -80,9 +80,8 @@ def load_inventory():
     inventory_df = pd.DataFrame(data, columns=['SKU', 'Stock', 'Ordered_Quantity', 'Arrival_Date'])
     return inventory_df
 
-# Datei-Pfade
-mapping_file_path = '/mnt/data/sku_mapping - Tabellenblatt1.csv'
-sales_file_path = '/mnt/data/salesbyarticle (54).xlsx'
+# URL der Zuordnungsdatei in Google Drive
+mapping_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFPFGMjeiiONwFjegJjsGRPDjtkW8bHRfqJX92a4P9k7yGsYjHGKuvpA1QNNrAI4eugweXxaDSeSwv/pub?output=csv"
 
 st.title("Datei-Uploader und Datenverarbeiter")
 
@@ -91,7 +90,7 @@ uploaded_file = st.file_uploader("Laden Sie eine Datei hoch", type=["xlsx"])
 create_or_update_table()
 
 if uploaded_file is not None:
-    mapping_df = load_mapping(mapping_file_path)
+    mapping_df = load_mapping(mapping_url)
     processed_data = process_file(uploaded_file, mapping_df)
     
     st.write("Verarbeitete Daten:")
