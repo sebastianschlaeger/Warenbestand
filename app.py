2440ce912955f16193d379a9e54c743a91239032
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-07-08 16:51:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ import pandas as pd
 app = Flask(__name__)
 
 # Konfiguration
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///inventory.db')
+database_url = os.environ.get('DATABASE_URL')
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['SECRET_KEY'] = 'your_secret_key'  # Für Flash-Nachrichten
