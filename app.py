dbe758cd445efd3cd4b87d631c3489257fda85d3
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-07-08 16:36:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,10 @@ def index():
     items = Item.query.all()
     return render_template('index.html', items=items)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+def init_db():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
+    init_db()
     app.run()
