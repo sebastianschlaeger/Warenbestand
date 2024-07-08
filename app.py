6a66b5da615422729217221a5e8ca690dbbc557b
sebastianschlaeger <122509871+sebastianschlaeger@users.noreply.github.com>
2024-07-08 16:35:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Item(db.Model):
+    __tablename__ = 'items'  # Explizit den Tabellennamen definieren
     id = db.Column(db.Integer, primary_key=True)
     sku = db.Column(db.String(50), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=False)
@@ -31,10 +32,9 @@ def index():
     items = Item.query.all()
     return render_template('index.html', items=items)
 
-def init_db():
-    with app.app_context():
-        db.create_all()
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 if __name__ == '__main__':
-    init_db()
-    app.run(debug=True)
+    app.run()
